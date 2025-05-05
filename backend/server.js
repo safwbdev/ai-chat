9d599bd630a5205872c6bc422ca3ac6ab73e2478
Stefaan <safwbdev@gmail.com>
2025-05-06 01:38:46 +0800

    possible fix

--- backend/server.js
+++ backend/server.js
@@ -160,9 +160,9 @@ app.use((err, req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
 
 app.listen(port, () => {
     connect()
