c34d7763fff4b02a5e6b2e377a6abf15ac191a26
Stefaan <safwbdev@gmail.com>
2025-05-08 00:35:22 +0800

    possible fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { CHAT_ID, DASHBOARD } from './routes.js';
 import Home from './pages/home/Home.jsx'
 import Dashboard from './pages/dashboard/Dashboard.jsx'
 import Chat from './pages/chat/Chat.jsx';
-import RootLayout from './layouts/rootLayout/rootLayout.jsx';
+import RootLayout from './layouts/rootLayout/RootLayout.jsx';
 import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx';
 import Login from './pages/login/Login.jsx';
 import Register from './pages/register/Register.jsx';
